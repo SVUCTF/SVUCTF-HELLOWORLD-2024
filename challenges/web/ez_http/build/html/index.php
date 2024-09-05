605a64b59b3e71cd982fa9d3b7e0829fc1ae8333
Yeol <136407746+13m0n4de@users.noreply.github.com>
2024-09-05 17:36:31 +0800

    feat(web/ez-http): 添加构建文件和题目信息

---
+++ challenges/web/ez_http/build/html/index.php
@@ -0,0 +1,16 @@
+<?php
+
+if (strpos($_SERVER['REMOTE_ADDR'], "127") === 0) {
+    if ($_SERVER["HTTP_REFERER"] === "genshin.edu.cn") {
+        if ($_SERVER["HTTP_USER_AGENT"] === "svuctf") {
+            echo "flag在小饼干里!";
+            setcookie("flag", file_get_contents("/flag"));
+        } else {
+            echo "请使用 svuctf 浏览器!";
+        }
+    } else {
+        echo "不是 genshin.edu.cn 来的我不要";
+    }
+} else {
+    echo "必须从本地访问!";
+}
