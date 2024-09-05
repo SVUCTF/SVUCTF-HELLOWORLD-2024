984f9924031e5d2f549b55feca06d723269d345f
Yeol <136407746+13m0n4de@users.noreply.github.com>
2024-09-06 01:25:09 +0800

    refactor(web/param): 优化代码结构

--- challenges/web/param/build/html/index.php
+++ challenges/web/param/build/html/index.php
@@ -1,19 +1,24 @@
 <?php
 
-$getParam = $_GET['svu'];
-$postParam = $_POST['ctf'];
-
-if (isset($getParam) && isset($postParam)) {
-    if ($getParam === "a") {
-        echo ("POST 方式上传参数 ctf，值为 b");
-        if ($postParam === "b") {
-            echo file_get_contents("/flag");
-        } else {
-            echo ("POST 参数错误!");
-        }
-    } else {
-        echo ("GET 参数错误!");
-    }
-} else {
-    echo ("GET 方式上传参数 svu，值为 a");
+if (!isset($_GET['svu'])) {
+    echo "GET 方式上传参数 svu，值为 a";
+    exit;
 }
+
+if ($_GET['svu'] !== "a") {
+    echo "GET 参数错误!";
+    exit;
+}
+
+echo "POST 方式上传参数 ctf，值为 b";
+
+if (!isset($_POST["ctf"])) {
+    exit;
+}
+
+if ($_POST['ctf'] !== "b") {
+    echo "POST 参数错误!";
+    exit;
+}
+
+echo file_get_contents("/flag");
