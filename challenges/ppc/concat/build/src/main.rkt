641db5ac5268ec9b738c296b1759a20e3429570a
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-09-11 22:59:04 +0800

    feat(ppc/concat): 添加 README 和源文件、构建文件 (#16)
    * docs(ppc/concat): 添加 README
    
    * feat(ppc/concat): 添加源代码
    
    * feat(ppc/concat): 添加镜像构建文件
    
    * refactor(ppc/concat): 高人指点，重构代码
    
    * docs(ppc/concat): 高人指点，更新题目描述

---
+++ challenges/ppc/concat/build/src/main.rkt
@@ -0,0 +1,42 @@
+#lang racket/base
+
+(require "parser.rkt")
+(require "test-cases.rkt")
+
+(define parse-error-responses
+  '("语法都不对，做不来的事就别轻易说出口"
+    "连语法都会出错，有办法背负其他人的人生吗"
+    "没有人拜托你那样写语法，这是最后的警告"))
+
+(define incorrect-responses
+  '("连题目的要求听不进去，你这个人真是满脑子都想着自己呢"
+    "今后不要再和我扯上关系了"))
+
+(define (random-response responses)
+  (list-ref responses (random (length responses))))
+
+(define (run-challenge [test-cases test-cases])
+  (if (null? test-cases)
+      (let ([flag (getenv "GZCTF_FLAG")])
+        (if flag
+            (displayln flag)
+            (displayln "错误：未找到 Flag 环境变量")))
+      (let* ([test-case (car test-cases)]
+             [description (car test-case)]
+             [expected (cadr test-case)]
+             [response (caddr test-case)])
+        (printf "祥子：~a\n" description)
+        (display "soyo：")
+        (with-handlers
+            ([exn:fail?
+              (lambda (_exn)
+                (printf "祥子：~a\n" (random-response parse-error-responses)))])
+          (let ([parsed (parse-concat-lang (read-line))])
+            (if (equal? parsed expected)
+                (begin
+                  (printf "祥子：~a\n" response)
+                  (run-challenge (cdr test-cases)))
+                (begin
+                  (printf "祥子：~a\n" (random-response incorrect-responses)))))))))
+
+(run-challenge)
