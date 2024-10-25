f5c89cf78dcbed2caa93067c80f861729d5aad54
Yeol <136407746+13m0n4de@users.noreply.github.com>
2024-10-26 03:54:28 +0800

    chore(crypto/binary-arithmetic): 添加附件生成脚本

---
+++ challenges/crypto/binary_arithmetic/build/gen.py
@@ -0,0 +1,20 @@
+import random
+
+flag = "SVUCTF{po_xiao_yyds}"
+
+print("|原字符|目标字符|原二进制|目标二进制|差值|")
+print("|-|-|-|-|-|")
+
+for flag_char in flag:
+    flag_ascii = ord(flag_char)
+    random_ascii = random.randint(32, flag_ascii)
+    ascii_diff = flag_ascii - random_ascii
+
+    orig_binary = random.randint(0, 255 - ascii_diff)
+    target_binary = orig_binary + ascii_diff
+
+    random_char = chr(random_ascii)
+    orig_bin_str = format(orig_binary, "08b")
+    target_bin_str = format(target_binary, "08b")
+
+    print(f"|{random_char}||{orig_bin_str}|{target_bin_str}||")
