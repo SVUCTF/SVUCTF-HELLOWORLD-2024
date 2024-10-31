1dae913ee9256b58f959601ecc233c856f602e25
Yeol <136407746+13m0n4de@users.noreply.github.com>
2024-11-01 06:01:34 +0800

    chore(build): 添加附件源码

---
+++ challenges/reverse/upx/build/upx.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void good() {
+    puts("ZmxhZ3s2YjRjMmU4NS04ZTcyLTQ2MWYtOTYxZi04MTQ1N2YxNDEzZmJ9Cg==");
+}
+
+int main() {
+    printf("ohh,you are good!!!");
+    return 0;
+}
