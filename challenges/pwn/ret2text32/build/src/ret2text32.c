7dce585ce7fb70d653ef2d475604c544007709f2
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-11-02 03:53:29 +0800

    feat(pwn/ret2text32): 添加题目 (#47)
    * docs(pwn/ret2text32): 添加 README
    
    * feat(pwn/ret2text32): 添加源码
    
    * feat(pwn/ret2text32): 添加镜像构建文件
    
    * ci(pwn/ret2text32): 添加工作流文件
    
    * chore(pwn/ret2text32): update attachments [skip ci]
    
    * style(pwn/ret2text32): 修改程序输出信息
    
    * chore(pwn/ret2text32): update attachments [skip ci]
    
    * refactor(pwn/ret2text32): 更新缓冲区大小
    
    * chore(pwn/ret2text32): update attachments [skip ci]
    
    * feat(pwn/ret2text32): 添加题解脚本
    
    * fix(pwn/ret2text32): 修复题解中填充大小的计算错误
    
    * docs(readme): 添加题解

---
+++ challenges/pwn/ret2text32/build/src/ret2text32.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void init() {
+    setvbuf(stdin, NULL, _IONBF, 0);
+    setvbuf(stdout, NULL, _IONBF, 0);
+    setvbuf(stderr, NULL, _IONBF, 0);
+}
+
+void banner() {
+    printf(" ___  _  _  __  __  ___  ____  ____ \n");
+    printf("/ __)( \\/ )(  )(  )/ __)(_  _)( ___)\n");
+    printf("\\__ \\ \\  /  )(__)(( (__   )(   )__) \n");
+    printf("(___/  \\/  (______)\\___) (__) (__) \n");
+    printf("\n");
+    printf("Welcome to the SVUCTF HELLOWORLD 2024!\n");
+    printf("\n");
+}
+
+void backdoor() {
+    system("/bin/sh");
+}
+
+void vuln() {
+    char buf[128];
+    printf("Program: Hey there! Think you can hack me?\n");
+    printf("You: ");
+    gets(buf);
+    printf("Program: Hmm, I feel a bit strange...\n");
+}
+
+int main() {
+    init();
+    banner();
+    vuln();
+    return 0;
+}
