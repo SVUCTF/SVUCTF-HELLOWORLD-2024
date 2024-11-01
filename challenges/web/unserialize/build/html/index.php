346205390a3448b72522a189e35eada1774630b6
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-11-02 07:06:54 +0800

    feat(web/unserialize): 添加题目 (#55)

---
+++ challenges/web/unserialize/build/html/index.php
@@ -0,0 +1,38 @@
+<?php
+
+highlight_file(__FILE__);
+
+class A
+{
+    public $str;
+    public function __invoke()
+    {
+        echo ($this->str);
+        echo ("还差一步，再看看");
+    }
+}
+
+class B
+{
+    public $fun;
+    public function __destruct()
+    {
+        ($this->fun)();
+    }
+}
+
+class C
+{
+    public $command;
+    public function __toString()
+    {
+        system($this->command);
+        return "好像执行了什么";
+    }
+}
+
+if (isset($_POST['data'])) {
+    unserialize($_POST['data']);
+} else {
+    echo ("你参数呢?");
+}
